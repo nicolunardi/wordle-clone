9346a656fe72113bebd4f93068fb535fd7faf8ac
lilnako <nlunardi@hotmail.com>
2023-02-27 14:47:33 +1100

    feat: added keyboard

--- components/Keyboard.tsx
+++ components/Keyboard.tsx
@@ -1,7 +1,9 @@
+import { BackspaceIcon } from "@heroicons/react/24/outline";
+
 const letters = [
   ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
   ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
-  ["enter", "z", "x", "c", "v", "b", "n", "m", "back"],
+  ["z", "x", "c", "v", "b", "n", "m"],
 ];
 
 function Keyboard() {
@@ -16,6 +18,36 @@ function Keyboard() {
             {letter}
           </button>
         ))}
+      </div>
+      <div className="flex gap-2">
+        {letters[1].map((letter, idx) => (
+          <button
+            className="border rounded flex justify-center items-center w-12 h-14 bg-gray-300 uppercase"
+            key={idx}
+          >
+            {letter}
+          </button>
+        ))}
+      </div>
+      <div className="flex gap-2">
+        {
+          <button className="border rounded flex justify-center items-center h-14 w-16 bg-gray-300 uppercase text-sm">
+            {"enter"}
+          </button>
+        }
+        {letters[2].map((letter, idx) => (
+          <button
+            className="border rounded flex justify-center items-center w-12 h-14 bg-gray-300 uppercase"
+            key={idx}
+          >
+            {letter}
+          </button>
+        ))}
+        {
+          <button className="border rounded flex justify-center items-center w-16 h-14 bg-gray-300 uppercase">
+            {<BackspaceIcon className="w-6 h-6" />}
+          </button>
+        }
       </div>
     </div>
   );
