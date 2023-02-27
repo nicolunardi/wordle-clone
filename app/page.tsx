0e9d499c27f06e888fcfdacf9820ca44e9d595d1
lilnako <nlunardi@hotmail.com>
2023-02-28 07:43:40 +1100

    feat: added colors for guesses

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Keyboard from "@/components/Keyboard";
 import WordGrid from "@/components/WordGrid";
 import { Poppins } from "next/font/google";
+import { useEffect, useState } from "react";
+import words from "@/data/words";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -8,15 +11,46 @@ const poppins = Poppins({
 });
 
 export default function Home() {
+  const [word, setWord] = useState<string>("");
+  const [allGuesses, setAllGuesses] = useState<string[]>([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
+  const [guesses, setGuesses] = useState<number>(0);
+
+  const handleKeyup = (e: KeyboardEvent) => {
+    const key = e.key;
+    const newGuesses = [...allGuesses];
+    console.log(newGuesses);
+    if (newGuesses[guesses].length < 5 && key.match(/^[A-z]$/)) {
+      newGuesses[guesses] = newGuesses[guesses] + key;
+      console.log(newGuesses);
+    }
+
+    setAllGuesses(newGuesses);
+  };
+
+  useEffect(() => {
+    setWord(words[Math.floor(Math.random() * words.length)]);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyup);
+
+    return () => window.removeEventListener("keyup", handleKeyup);
+  }, [handleKeyup]);
+
   return (
     <main className="p-5 flex-1 flex flex-col items-center">
       {/* grid */}
       <div className="flex flex-col gap-2">
-        {Array.from(
-          Array(6)
-            .fill(0)
-            .map((row, idx) => <WordGrid key={idx} />)
-        )}
+        {allGuesses.map((guess, idx) => (
+          <WordGrid guess={guess} word={word} key={idx} />
+        ))}
       </div>
       {/* keyboard */}
       <div className="w-full">
--- components/WordGrid.tsx
+++ components/WordGrid.tsx
@@ -1,12 +1,30 @@
-function WordGrid() {
+"use client";
+interface Props {
+  word: string;
+  guess: string;
+}
+
+function WordGrid({ word, guess }: Props) {
   return (
     <div className="flex gap-2 4">
-      {Array.from(Array(5).fill(0)).map((row, idx) => (
-        <div
-          className="h-14 w-14 border rounded flex justify-center items-center"
-          key={idx}
-        ></div>
-      ))}
+      {Array.from(Array(5).fill(0)).map((_, idx) => {
+        const bgColor = !guess[idx]
+          ? ""
+          : word[idx] === guess[idx]
+          ? "bg-green-400"
+          : word.includes(guess[idx])
+          ? "bg-yellow-300"
+          : "bg-gray-500";
+        return (
+          <div
+            className={`h-14 w-14 border rounded flex justify-center items-center ${bgColor} uppercase`}
+            key={idx}
+          >
+            {guess[idx]}
+          </div>
+        );
+      })}
+      <h1>{word}</h1>
     </div>
   );
 }
